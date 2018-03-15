ccee4b71073f384567aefb8d8c6c907aa7818dbe
Karthick Mahalingam <itkarthick@hotmail.com>
2018-03-15 13:37:21 -0700

    fix indentation and remove class variables

--- src/main/java/com/zipcode/processor/App.java
+++ src/main/java/com/zipcode/processor/App.java
@@ -3,26 +3,20 @@ package com.zipcode.processor;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    private static Scanner scan;
-    
-	public static void main( String[] args )
-    {
-        scan = new Scanner(System.in);
-        String zipcodeRanges = scan.nextLine();
-        ZipcodeProcessor zipcodeProcessor = new ZipcodeProcessor(zipcodeRanges);
-        List<Zipcode> zipcodeList = zipcodeProcessor.stripZipcode();
-        ZipcodeMerger zipcode_merger = new ZipcodeMerger(zipcodeList);
-        List<Zipcode> mergedZipcodeList = zipcode_merger.mergeZipcodes();
-        for(Zipcode zipcode: mergedZipcodeList) {
-        	System.out.println("["+zipcode.getLower_bound()+","+zipcode.getUpper_bound() +"]");
-        }
-        
-        
+public class App {
+  private static Scanner scan;
+
+  public static void main(String[] args) {
+    scan = new Scanner(System.in);
+    String zipcodeRanges = scan.nextLine();
+    ZipcodeProcessor zipcodeProcessor = new ZipcodeProcessor(zipcodeRanges);
+    List<Zipcode> zipcodeList = zipcodeProcessor.stripZipcode();
+    ZipcodeMerger zipcode_merger = new ZipcodeMerger();
+    List<Zipcode> sortedZipCodeList = zipcode_merger.sortByLowerBounds(zipcodeList);
+    List<Zipcode> mergedZipcodeList = zipcode_merger.mergeZipcodes(sortedZipCodeList);
+    for (Zipcode zipcode : mergedZipcodeList) {
+      System.out.println("[" + zipcode.getLower_bound() + "," + zipcode.getUpper_bound() + "]");
     }
+
+  }
 }
--- src/main/java/com/zipcode/processor/ZipcodeComparator.java
+++ src/main/java/com/zipcode/processor/ZipcodeComparator.java
@@ -3,10 +3,10 @@ package com.zipcode.processor;
 import java.util.Comparator;
 
 public class ZipcodeComparator implements Comparator<Zipcode> {
-	public int compare(Zipcode interval1, Zipcode interval2) {
-		if(interval1.getLower_bound() > interval2.getLower_bound()) 
-			return 1;
-		else
-			return -1;
-	}
+  public int compare(Zipcode interval1, Zipcode interval2) {
+    if (interval1.getLower_bound() > interval2.getLower_bound())
+      return 1;
+    else
+      return -1;
+  }
 }
